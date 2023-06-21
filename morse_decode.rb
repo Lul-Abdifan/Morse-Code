21281ba5c5d74cf42270e61083987df64086d00e
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-21 21:57:25 +0000

    Align the dictionary keys and values for better readability

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,37 +1,15 @@
 MORSE_CODE_TABLE = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
+  '.-': 'A',   '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+  '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+  '-.-': 'K',  '.-..': 'L', '--': 'M',   '-.': 'N', '---': 'O',
+  '.--.': 'P', '--.-': 'Q', '.-.': 'R',  '...': 'S', '-': 'T',
+  '..-': 'U',  '...-': 'V', '.--': 'W',   '-..-': 'X', '-.--': 'Y',
+  '--..': 'Z'
 }.freeze
 
 def decode_char(character)
   MORSE_CODE_TABLE[character]
 end
-
-puts decode_char('.-')
 
 def decode_word(word)
   decoded_word = ''
@@ -43,13 +21,13 @@ def decode_word(word)
   decoded_word
 end
 
-puts decode_word('-- -.--')
-
 def decode(statement)
   split_statement = statement.split('   ')
   overall = split_statement.map { |word| decode_word(word) }
   overall.join(' ')
 end
 
+puts decode_char('.-')
+puts decode_word('-- -.--')
 puts decode('-- -.--   -. .- -- .')
 puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
