ca546ba5556163cb769596af596df5414e8487eb
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-21 22:10:35 +0000

    Use more descriptive variable names to make the code easier to understand

--- morse_decode.rb
+++ morse_decode.rb
@@ -16,19 +16,19 @@ end
 # Decodes a single Morse code word
 def decode_word(word)
   decoded_word = ''
-  split_word = word.split
-  split_word.each do |character|
-    decoded_character = MORSE_CODE_TABLE[character]
-    decoded_word += decoded_character if decoded_character
+  chars = word.split
+  chars.each do |character|
+    letter = MORSE_CODE_TABLE[character]
+    decoded_word += letter if letter
   end
   decoded_word
 end
 
 # Decodes a full Morse code statement
 def decode(statement)
-  split_statement = statement.split('   ')
-  overall = split_statement.map { |word| decode_word(word) }
-  overall.join(' ')
+  words = statement.split('   ')
+  decoded_words = words.map { |word| decode_word(word) }
+  decoded_words.join(' ')
 end
 
 # Example usage
