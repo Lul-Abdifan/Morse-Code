7e5c20d9e4e5ed291aa15b7c2d8fe78e252fa04b
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-21 09:08:51 -0400

    Fix linters error

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,39 +1,41 @@
 MORSE_CODE_TABLE = {
-  '.-'    => 'A',
-  '-...'  => 'B',
-  '-.-.'  => 'C',
-  '-..'   => 'D',
-  '.'     => 'E',
-  '..-.'  => 'F',
-  '--.'   => 'G',
-  '....'  => 'H',
-  '..'    => 'I',
-  '.---'  => 'J',
-  '-.-'   => 'K',
-  '.-..'  => 'L',
-  '--'    => 'M',
-  '-.'    => 'N',
-  '---'   => 'O',
-  '.--.'  => 'P',
-  '--.-'  => 'Q',
-  '.-.'   => 'R',
-  '...'   => 'S',
-  '-'     => 'T',
-  '..-'   => 'U',
-  '...-'  => 'V',
-  '.--'   => 'W',
-  '-..-'  => 'X',
-  '-.--'  => 'Y',
-  '--..'  => 'Z'
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def decode_char(character)
-  return MORSE_CODE_TABLE[character]
+  MORSE_CODE_TABLE[character]
 end
 
+puts decode_char('.-')
+
 def decode_word(word)
-  decoded_word = ""
-  split_word = word.split(" ")
+  decoded_word = ''
+  split_word = word.split
   split_word.each do |character|
     more_match = MORSE_CODE_TABLE[character]
     decoded_word += more_match if more_match
@@ -41,14 +43,13 @@ def decode_word(word)
   decoded_word
 end
 
+puts decode_word('-- -.--')
+
 def decode(statement)
-  split_statement = statement.split("   ")
-  overall_word = split_statement.map { |word| decode_word(word) }
-  decoded_words = overall_word.join(" ")
-  decoded_words
+  split_statement = statement.split('   ')
+  overall = split_statement.map { |word| decode_word(word) }
+  overall.join(' ')
 end
 
-puts decode_char(".-")
-puts decode("-- -.--   -. .- -- .")
-puts decode_word("-. .- -- .")
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
